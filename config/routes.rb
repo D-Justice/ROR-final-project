cfe910aef8ae24e640a2cae9dfe6f57e133f0d45
Damian Justice <damian.justice@globalx.com.au>
2022-05-10 17:43:28 +1000

    add login and signup routes and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get "/home", to: "index#index"
   resources :posts, only: [:index, :new, :create]
+  resources :login, only: [:index]
+  resources :signup, only: [:index, :new, :create]
+  
 end
